c9290392083ccde2855e196c0f4435e8cdbf6daf
Shrutie03 <epilishruti2003@gmail.com>
2023-10-16 20:33:33 +0530

    Initial commit

---
+++ src/com/internshala/javaapp/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//implicit conversion
+
+//        int radi = 300;
+//        long newradi = radi;
+//        System.out.println(newradi);
+//        //explicit conversion
+//        double diameter = 3.40d;
+//        int newdiameter = (int) diameter;
+//        System.out.println(newdiameter);
+
+        String name ;
+        int population ;
+        float popdensity;
+        double GDP;
+        char currency;
+
+    }
+}
